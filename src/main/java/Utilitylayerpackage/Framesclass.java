6bf2aaf2fb3f626dd5a363b04d75603200eb6471
HARSHINI PENDOTA <HARSHINI PENDOTA@DESKTOP-36E45VO>
2024-07-16 22:08:37 +0530

    Framesclass created

---
+++ src/main/java/Utilitylayerpackage/Framesclass.java
@@ -0,0 +1,9 @@
+package Utilitylayerpackage;
+
+public class Framesclass {
+
+	public static void main(String[] args) {
+    System.out.println("this is the second workplace adding frames class utility package");
+	}
+
+}
