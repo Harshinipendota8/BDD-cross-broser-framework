6c98dbef43bf12b20241fa3219b427f68f2a37a9
unknown <Harshinipendota8>
2025-06-02 12:22:02 +0530

    added newpage java file

---
+++ src/test/java/TestLayer/newpage.java
@@ -0,0 +1,13 @@
+package TestLayer;
+
+import org.testng.annotations.Test;
+
+public class newpage {
+	
+@Test
+public void display()
+{
+	System.out.println("this is to add a new file in git");
+}
+
+}
